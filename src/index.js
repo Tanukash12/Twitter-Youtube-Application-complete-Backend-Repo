27f03d4d0b078fa02ed97482cb95db77186b0c2c
Tanushka12 <kashyaptanushka14@gmail.com>
2025-07-07 15:37:39 +0530

    utis code added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
 
-
-
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.error("Server error:", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
